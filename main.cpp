2263a4c61f3d4ef43380660841589ae46d71853b
Alexander Kedrik <alexander.kedrik@gmail.com>
2013-12-31 00:53:33 +0400

    refactoring

--- main.cpp
+++ main.cpp
@@ -48,7 +48,7 @@ using PathList = std::vector<boost::filesystem::path>;   // can include files an
 
 class PlaylistModel : public QAbstractTableModel {
 public:
-	PlaylistModel(const QMediaPlaylist & playlist) : playlist_(playlist) {
+	PlaylistModel(QMediaPlaylist * playlist) : playlist_(playlist) {
 	}
 
 	int columnCount(const QModelIndex & parent) const override {
@@ -72,13 +72,14 @@ public:
 
 	int rowCount(const QModelIndex & parent) const override {
 		if (parent.isValid()) return 0;
-		return playlist_.mediaCount();
+		return playlist_->mediaCount();
 	}
 
 	QVariant data(const QModelIndex & index, int role) const override {
+		// TODO: colors
 		if (role != Qt::DisplayRole) return QVariant();
 
-		const auto & media = playlist_.media(index.row());
+		const auto & media = playlist_->media(index.row());
 		switch (index.column()) {
 			case 0: return media.canonicalUrl();
 			default: return QVariant();
@@ -86,21 +87,21 @@ public:
 	}
 
 private:
-	const QMediaPlaylist & playlist_;
+	QMediaPlaylist * playlist_;
 };
 
 
-std::string millisecondsToString(qint64 val) {
+QString millisecondsToString(qint64 val) {
 	uint8_t seconds = (uint8_t)((val / 1000) % 60);
 	uint8_t minutes = (uint8_t)((val / 1000 / 60) % 60);
 	uint16_t hours = (uint16_t)(val / 1000 / 60 / 60);
 
-	std::string res;
-	if (hours > 0) res += std::to_string(hours) + ":";
+	QString res;
+	if (hours > 0) res += QString::number(hours) + ":";
 	if ((hours > 0) && (minutes < 10)) res += "0";
-	res += std::to_string(minutes) + ":";
+	res += QString::number(minutes) + ":";
 	if (seconds < 10) res += "0";
-	res += std::to_string(seconds);
+	res += QString::number(seconds);
 	return res;
 }
 
@@ -129,177 +130,189 @@ protected:
 	}
 };
 
-class MainWindow : public QMainWindow {
-	QMediaPlayer player;
 
-	QWidget mainWidget;
-	QVBoxLayout mainLayout;
-		QHBoxLayout firstLineLayout;
-			QPushButton playPauseButton;
-			QPushButton stopButton;
-			QPushButton nextButton;
-			QPushButton prevButton;
-			QPushButton muteButton;
-			QPushButton repeatButton;
-			QPushButton randomizeButton;
-			MySlider volumeSlider;
-		QHBoxLayout secondLineLayout;
-			MySlider timeSlider;
-			QLabel timeLabel;
-		QTableView playlistTable;
-		PlaylistModel playlistModel;
-		QMediaPlaylist playlist;
-
+class ButtonsAndVolumeControlWidget : public QWidget {
 public:
-
-	MainWindow()
- 	: playPauseButton(QIcon::fromTheme("media-playback-start"), "")
-	, stopButton(QIcon::fromTheme("media-playback-stop"), "")
-	, nextButton(QIcon::fromTheme("go-next"), "")
-	, prevButton(QIcon::fromTheme("go-previous"), "")
-	, muteButton(QIcon::fromTheme("audio-volume-muted"), "")
-	, repeatButton(QIcon::fromTheme("media-playlist-repeat"), "")
-	, randomizeButton(QIcon::fromTheme("media-playlist-shuffle"), "")
-	, timeLabel("0:00 / 0:00")
-	, playlistModel(playlist)
-	{
-		playPauseButton.setCheckable(true);
-		playPauseButton.setShortcut(QKeySequence("p"));
-		stopButton.setCheckable(true);
-		stopButton.setShortcut(QKeySequence("s"));
-		muteButton.setCheckable(true);
-		muteButton.setShortcut(QKeySequence("m"));
-		repeatButton.setCheckable(true);
-		repeatButton.setShortcut(QKeySequence("r"));
-		randomizeButton.setCheckable(true);
-		randomizeButton.setShortcut(QKeySequence("Shift+r"));
-
-		volumeSlider.setOrientation(Qt::Horizontal);
-		volumeSlider.setMaximum(100);
-		timeSlider.setOrientation(Qt::Horizontal);
-		timeSlider.setTracking(false);
-
-		playlist.addMedia(QUrl::fromLocalFile("/home/alkedr/music/zero-project/zero-project - 02 - Gothic.ogg"));
-
-		playlistTable.setModel(&playlistModel);
-
-		setCentralWidget(&mainWidget);
-		mainWidget.setLayout(&mainLayout);
-			mainLayout.addLayout(&firstLineLayout, 0);
-				firstLineLayout.addWidget(&playPauseButton);
-				firstLineLayout.addWidget(&stopButton);
-				firstLineLayout.addWidget(&nextButton);
-				firstLineLayout.addWidget(&prevButton);
-				firstLineLayout.addWidget(&muteButton);
-				firstLineLayout.addWidget(&repeatButton);
-				firstLineLayout.addWidget(&randomizeButton);
-				firstLineLayout.addWidget(&volumeSlider, 1);
-			mainLayout.addLayout(&secondLineLayout, 0);
-				secondLineLayout.addWidget(&timeSlider);
-				secondLineLayout.addWidget(&timeLabel);
-			mainLayout.addWidget(&playlistTable, 1);
+	ButtonsAndVolumeControlWidget(QMediaPlayer * player) {
+		auto mainLayout = new QHBoxLayout(this);
+			auto playPauseButton = createCheckableButton("media-playback-start", "p", mainLayout);
+			auto stopButton = createCheckableButton("media-playback-stop", "s", mainLayout);
+			auto prevButton = createButton("go-previous", "", mainLayout);
+			auto nextButton = createButton("go-next", "", mainLayout);
+			auto muteButton = createCheckableButton("audio-volume-muted", "m", mainLayout);
+			auto repeatButton = createCheckableButton("media-playlist-repeat", "r", mainLayout);
+			auto randomizeButton = createCheckableButton("media-playlist-shuffle", "Shift+r", mainLayout);
+			auto volumeSlider = createSlider(mainLayout);
 
 
-		connect(&player, &QMediaPlayer::stateChanged,
-			[this](QMediaPlayer::State newState) {
+		connect(player, &QMediaPlayer::stateChanged,
+			[playPauseButton, stopButton](QMediaPlayer::State newState) {
 				if (newState == QMediaPlayer::StoppedState) {
-					playPauseButton.setChecked(false);
-					stopButton.setChecked(true);
+					std::cout << "QMediaPlayer::stateChanged to Stopped" << std::endl;
+					playPauseButton->setChecked(false);
+					stopButton->setChecked(true);
 				} else if (newState == QMediaPlayer::PlayingState) {
-					playPauseButton.setChecked(true);
-					stopButton.setChecked(false);
+					std::cout << "QMediaPlayer::stateChanged to Playing" << std::endl;
+					playPauseButton->setChecked(true);
+					stopButton->setChecked(false);
 				} else if (newState == QMediaPlayer::PausedState) {
-					playPauseButton.setChecked(false);
-					stopButton.setChecked(false);
+					std::cout << "QMediaPlayer::stateChanged to Paused" << std::endl;
+					playPauseButton->setChecked(false);
+					stopButton->setChecked(false);
 				}
 			}
 		);
 
-		connect(&player, &QMediaPlayer::positionChanged,
-			[this](qint64 newPosition) {
-				timeSlider.blockSignals(true);
-				timeSlider.setSliderPosition((qint32)newPosition);
-				timeSlider.blockSignals(false);
-				timeLabel.setText(QString::fromStdString(millisecondsToString(newPosition) + " / " + millisecondsToString(player.duration())));
+		connect(player, &QMediaPlayer::volumeChanged,
+			[volumeSlider](int newVolume) {
+				volumeSlider->blockSignals(true);
+				volumeSlider->setSliderPosition(newVolume);
+				volumeSlider->blockSignals(false);
 			}
 		);
 
-		connect(&player, &QMediaPlayer::durationChanged,
-			[this](qint64 newDuration) {
-				timeSlider.setMaximum((qint32)newDuration);
-				timeLabel.setText(QString::fromStdString(millisecondsToString(player.position()) + " / " + millisecondsToString(newDuration)));
+		connect(playPauseButton, &QPushButton::clicked,
+			[player]() {
+				// TODO check button state instead
+				if (player->state() == QMediaPlayer::PlayingState) {
+					player->pause();
+				} else {
+					player->play();
+				}
 			}
 		);
 
-		connect(&player, &QMediaPlayer::volumeChanged,
-			[this](int newVolume) {
-				volumeSlider.blockSignals(true);
-				volumeSlider.setSliderPosition(newVolume);
-				volumeSlider.blockSignals(false);
+		connect(stopButton, &QPushButton::clicked,
+			[player]() {
+				// TODO check button state
+				player->stop();
 			}
 		);
 
-		connect(&player, &QMediaPlayer::setPlaylist,
+		connect(prevButton, &QPushButton::clicked,
+			[]() {
+			}
+		);
+
+		connect(nextButton, &QPushButton::clicked,
+			[]() {
+			}
+		);
+
+		connect(muteButton, &QPushButton::clicked,
+			[player, muteButton]() {
+				player->setMuted(muteButton->isChecked());
+			}
+		);
+
+
+		connect(volumeSlider, &MySlider::valueChanged,
+			[player](int newValue) {
+				player->setVolume(newValue);
+			}
+		);
+	}
+
+private:
+	static QPushButton * createButton(const char* iconName, const char* keySequenceString, QLayout * layout) {
+		auto res = new QPushButton(QIcon::fromTheme(iconName), iconName);
+		res->setShortcut(QKeySequence(keySequenceString));
+		layout->addWidget(res);
+		return res;
+	}
+
+	static QPushButton * createCheckableButton(const char* iconName, const char* keySequenceString, QLayout * layout) {
+		auto res = createButton(iconName, keySequenceString, layout);
+		res->setCheckable(true);
+		return res;
+	}
+
+	static QSlider * createSlider(QLayout * layout) {
+		auto res = new MySlider;
+		res->setOrientation(Qt::Horizontal);
+		layout->addWidget(res);
+		return res;
+	}
+};
+
+
+class TimeControlWidget : public QWidget {
+public:
+	TimeControlWidget(QMediaPlayer * player) {
+		auto mainLayout = new QHBoxLayout(this);
+			auto timeSlider = new MySlider();
+			auto timeLabel = new QLabel("0:00 / 0:00");
+
+		mainLayout->addWidget(timeSlider);
+		mainLayout->addWidget(timeLabel);
+
+		timeSlider->setOrientation(Qt::Horizontal);
+		timeSlider->setTracking(false);
+
+
+		connect(timeSlider, &MySlider::valueChanged,
+			[player](int newValue) {
+				player->setPosition(newValue);
+			}
+		);
+
+		connect(player, &QMediaPlayer::positionChanged,
+			[timeSlider, timeLabel, player](qint64 newPosition) {
+				timeSlider->blockSignals(true);
+				timeSlider->setSliderPosition((qint32)newPosition);
+				timeSlider->blockSignals(false);
+				timeLabel->setText(millisecondsToString(newPosition) + " / " + millisecondsToString(player->duration()));
+			}
+		);
+
+		connect(player, &QMediaPlayer::durationChanged,
+			[timeSlider, timeLabel, player](qint64 newDuration) {
+				timeSlider->setMaximum((qint32)newDuration);
+				timeLabel->setText(millisecondsToString(player->position()) + " / " + millisecondsToString(newDuration));
+			}
+		);
+	}
+};
+
+QMediaPlaylist * loadPlaylist() {
+	QMediaPlaylist * playlist = new QMediaPlaylist();
+	playlist->addMedia(QUrl::fromLocalFile("/home/alkedr/music/zero-project/zero-project - 02 - Gothic.ogg"));
+	return playlist;
+}
+
+class PlaylistWidget : public QTableView {
+public:
+	PlaylistWidget(QMediaPlayer * player) {
+		QMediaPlaylist * playlist = loadPlaylist();
+		setModel(new PlaylistModel(playlist));
+		player->setPlaylist(playlist);
+
+
+		/*connect(player, &QMediaPlayer::setPlaylist,
 			[this](QMediaPlaylist * newPlaylist) {
 			}
-		);
+		);*/
 
-
-		connect(&playPauseButton, &QPushButton::clicked,
-			[this]() {
-				if (player.state() == QMediaPlayer::PlayingState) {
-					player.pause();
-				} else if (player.state() == QMediaPlayer::PausedState) {
-					player.play();
-				}
-			}
-		);
-
-		connect(&stopButton, &QPushButton::clicked,
-			[this]() {
-				player.stop();
-			}
-		);
-
-		connect(&prevButton, &QPushButton::clicked,
-			[this]() {
-			}
-		);
-
-		connect(&nextButton, &QPushButton::clicked,
-			[this]() {
-			}
-		);
-
-		connect(&muteButton, &QPushButton::clicked,
-			[this]() {
-				player.setMuted(muteButton.isChecked());
-			}
-		);
-
-
-		connect(&volumeSlider, &MySlider::valueChanged,
-			[this](int newValue) {
-				player.setVolume(newValue);
-			}
-		);
-
-		connect(&timeSlider, &MySlider::valueChanged,
-			[this](int newValue) {
-				player.setPosition(newValue);
-			}
-		);
-
-
-		connect(&playlistTable, &QAbstractItemView::activated,
-			[this](const QModelIndex & index) {
+		connect(this, &QAbstractItemView::activated,
+			[player](const QModelIndex & index) {
 				if (index.isValid()) {
-					player.playlist()->setCurrentIndex(index.row());
+					player->playlist()->setCurrentIndex(index.row());
 				}
 			}
 		);
+	}
+};
 
-		player.setPlaylist(&playlist);
+
+class MainWindow : public QWidget {
+	QMediaPlayer player;
+
+public:
+	MainWindow() {
+		auto mainLayout = new QVBoxLayout(this);
+			mainLayout->addWidget(new ButtonsAndVolumeControlWidget(&player));
+			mainLayout->addWidget(new TimeControlWidget(&player));
+			mainLayout->addWidget(new PlaylistWidget(&player));
 
 		player.setNotifyInterval(50);
 		player.setVolume(50);
@@ -309,17 +322,13 @@ public:
 	~MainWindow() {
 		player.stop();
 	}
-
 };
 
 }
 
+
 int main(int argc, char ** argv) {
 	QApplication app(argc, argv);
-
-	MainWindow mainWindow;
-	mainWindow.show();
-
+	(new MainWindow())->show();
 	return app.exec();
 }
-
